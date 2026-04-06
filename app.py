eaa035dd2037b0bc957cbd33bacc78a87ab808df
andreyzza2011-ux <andrey.zza2011@gmail.com>
2026-04-06 21:07:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
     return "Bot is online!"
 
 def run_flask():
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 2022))
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
